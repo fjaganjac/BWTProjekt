50e8b4f7cf22f3023f0cb771b08a561ebd25d342
fjaganjac <fjaganjac1@etf.unsa.ba>
2022-11-09 18:04:45 +0100

    napisani konstruktori klase predmet/prisustvo

---
+++ predmet.js
@@ -0,0 +1,7 @@
+class Predmet {
+    
+    constructor() {
+        this.kodPredmeta = "";     
+    }
+
+}
---
+++ prisustvo.js
@@ -0,0 +1,13 @@
+class Prisustvo {
+
+    static trenutnaSedmica
+    prisustvo
+    finalnoStanje
+
+    constructor() {
+        this.trenutnaSedmica = 0;
+        this.prisustvo = 1;
+        this.finalnoStanje = false;
+    }
+
+}

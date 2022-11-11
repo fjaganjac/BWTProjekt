83b9d5493c7f125f3aa1b74e221b283b1eeb24ab
fjaganjac <fjaganjac1@etf.unsa.ba>
2022-11-11 17:41:07 +0100

    implementirana metoda provjeriKodPredmeta

--- predmet.js
+++ predmet.js
@@ -4,4 +4,17 @@ class Predmet {
         this.kodPredmeta = "";     
     }
 
+    provjeriKodPredmeta(kod) {
+        if(kod.split("-").length != 4 ||
+           !["RI", "AE", "EE", "TK"].includes(kod.split("-")[0]) ||
+           !["BoE", "MoE", "RS"].includes(kod.split("-")[1]) ||
+           !["1", "2", "3"].includes(kod.split("-")[2]) ||
+           !["1", "2"].includes(kod.split("-")[3]) ||
+           (["MoE", "RS"].includes(kod.split("-")[1]) && !["1", "2"].includes(kod.split("-")[2]))) {
+            return false;
+        }
+        this.kodPredmeta = kod;
+        return true;
+    }
+
 }

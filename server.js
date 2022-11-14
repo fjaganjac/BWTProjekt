6395c3ae71cebdac436dcac50ca1fb03f9ab113f
fjaganjac <fjaganjac1@etf.unsa.ba>
2022-11-14 14:22:01 +0100

    napisana /prisustvo POST ruta

--- server.js
+++ server.js
@@ -64,5 +64,67 @@ const server = http.createServer(function (req, res) {
         });
     }
 
+    else if (req.url == '/prisustvo' && req.method.toUpperCase() == 'POST') {  //prisustvo = {tipCasa:string,redniBrojCasa:integer,sedmica:integer,kodPredmeta:string,indexStudenta:string,statusPrisustva:string}
+        let buffer = '';
+        req.on('data', function (data) {
+            buffer += data;
+        });
+        req.on('end', function () {
+            const payload = JSON.parse(buffer);
+
+            if (!["prisutan", "odsutan", "nijeUneseno"].includes(payload.statusPrisustva)) {
+                res.writeHead(201, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ status: `Status prisustva nije ispravan!` }));
+            }
+            else {
+                {
+                    fs.readFile('./archives/predmeti.csv', function (err, data) {
+                        if (err) throw err;
+
+                        if (!data.includes(`,${payload.kodPredmeta}`)) {
+                            res.writeHead(201, { 'Content-Type': 'application/json' });
+                            res.end(JSON.stringify({ status: `Kod predmeta ne postoji!` }));
+                        }
+                    })
+                }
+                {
+                    fs.readFile('./archives/studenti.csv', function (err, data) {
+                        if (err) throw err;
+
+                        if (!data.includes(`,${payload.indexStudenta}\n`)) {
+                            res.writeHead(201, { 'Content-Type': 'application/json' });
+                            res.end(JSON.stringify({ status: `Student ne postoji!` }));
+                        }
+                    })
+                }
+                {
+                    fs.readFile('./archives/prisustva.csv', function (err, data) {
+                        if (err) throw err;
+                        var noviUpis = []
+                        if (data.includes(`${payload.tipCasa},${payload.redniBrojCasa},${payload.sedmica},${payload.kodPredmeta},${payload.indexStudenta},`)) {
+                            for (var red of data.toString().split('\n')) {
+                                red = (red.includes(`${payload.tipCasa},${payload.redniBrojCasa},${payload.sedmica},${payload.kodPredmeta},${payload.indexStudenta},`)) ? red.replace(/odsutan(?!,)|prisutan(?!,)|nijeUneseno(?!,)/gm, payload.statusPrisustva) : red
+                                noviUpis.push(red)
+                            }
+                            Upis = '';
+                            for (let red in noviUpis) { Upis += noviUpis[red] + ((red == noviUpis.length - 1) ? '' : '\n'); }
+                            fs.writeFile('./archives/prisustva.csv', Upis, (err) => {
+                                if (err) throw err;
+                            })
+                            res.writeHead(201, { 'Content-Type': 'application/json' });
+                            res.end(JSON.stringify({ status: `Izmjena napravljena!` }));
+                        }
+                        else {
+                            fs.appendFile('./archives/prisustva.csv', `${payload.tipCasa},${payload.redniBrojCasa},${payload.sedmica},${payload.kodPredmeta},${payload.indexStudenta},${payload.statusPrisustva}\n`, function (err) {
+                                if (err) throw err;
+                                res.writeHead(201, { 'Content-Type': 'application/json' });
+                                res.end(JSON.stringify({ status: "Kreirano prisustvo!" }));
+                            });
+                        }
+                    })
+                }
+            }
+        });
+    }
 }).listen(8080);
 console.log("slusam port 8080");

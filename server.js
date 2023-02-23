f71d8949de18277a9a53e280009efe9aa9824d9d
fjaganjac <fjaganjac1@etf.unsa.ba>
2023-02-23 01:48:14 +0100

    prepravljen server u skladu sa db

--- server.js
+++ server.js
@@ -1,161 +1,203 @@
-const fs = require('fs');
-const http = require('http');
-const url = require('url');
+const express = require('express');
+const parser = require('body-parser');
+const app = express();
 const Predmet = require('./scripts/predmet');
+const db = require('./db');
 
-const server = http.createServer(function (req, res) {
-    if (req.url == '/student' && req.method.toUpperCase() == 'POST') {
-        let buffer = '';
-        req.on('data', function (data) {
-            buffer += data;
-        });
-        req.on('end', function () {
-            const payload = JSON.parse(buffer);
+app.use(express.static('public'));
+app.use(express.static('public/prisustvo'));
+app.use(express.static('public/unosPredmeta'));
+app.use(parser.json());
 
-            fs.readFile('./archives/studenti.csv', function (err, data) {
-                if (err) throw err;
-
-                if (data.includes(`,${payload.index}\n`)) {
-                    res.writeHead(201, { 'Content-Type': 'application/json' });
-                    res.end(JSON.stringify({ status: `Student sa indexom ${payload.index} već postoji!` }));
-                }
-                else {
-                    let novaLinija = `${payload.ime},${payload.prezime},${payload.index}\n`;
-                    fs.appendFile('./archives/studenti.csv', novaLinija, function (err) {
-                        if (err) throw err;
-                        res.writeHead(201, { 'Content-Type': 'application/json' });
-                        res.end(JSON.stringify({ status: "Kreiran student!" }));
-                    });
-                }
+app.post('/student', async (req, res) => {
+    const payload = req.body;
+    await db.student.findOne({
+        where: {
+            index: payload.index
+        }
+    }).catch(err => {
+        console.log("error -> db.student.findOne: " + err);
+    }).then(studentPostoji => {
+        if (studentPostoji) {
+            res.status(209).send({ status: `Student sa indexom ${payload.index} već postoji` });
+        }
+        else {
+            db.student.create({
+                ime: payload.ime,
+                prezime: payload.prezime,
+                index: payload.index
             });
-        });
+            res.status(200).send({ status: "Kreiran student!" });
+        }
+    }).catch(err => {
+        console.log("error -> db.student.create: " + err);
+    });
+});
+
+app.post('/predmet', async (req, res) => {
+    const payload = req.body;
+    var predmet = new Predmet();
+    if (!predmet.provjeriKodPredmeta(payload.kod)) {
+        res.status(400).json({ status: "Kod predmeta nije ispravan" });
     }
-
-    else if (req.url == '/predmet' && req.method.toUpperCase() == 'POST') {
-        let buffer = '';
-        req.on('data', function (data) {
-            buffer += data;
-        });
-        req.on('end', function () {
-            const payload = JSON.parse(buffer);
-
-            fs.readFile('./archives/predmeti.csv', function (err, data) {
-                if (err) throw err;
-
-                if (data.includes(`,${payload.kod}`)) {
-                    res.writeHead(201, { 'Content-Type': 'application/json' });
-                    res.end(JSON.stringify({ status: `Predmet sa kodom ${payload.kod} vec postoji` }));
-                }
-                else {
-                    var predmet = new Predmet();
-                    if (!predmet.provjeriKodPredmeta(payload.kod)) {
-                        res.writeHead(201, { 'Content-Type': 'application/json' });
-                        res.end(JSON.stringify({ status: `Kod predmeta nije ispravan` }));
-                    }
-                    else {
-                        let novaLinija = `${payload.naziv},${payload.kod}\n`;
-                        fs.appendFile('./archives/predmeti.csv', novaLinija, function (err) {
-                            if (err) throw err;
-                            res.writeHead(201, { 'Content-Type': 'application/json' });
-                            res.end(JSON.stringify({ status: "Kreiran predmet!" }));
-                        });
-                    }
-                }
-            });
-        });
-    }
-
-    else if (req.url == '/prisustvo' && req.method.toUpperCase() == 'POST') {
-        let buffer = '';
-        req.on('data', function (data) {
-            buffer += data;
-        });
-        req.on('end', function () {
-            const payload = JSON.parse(buffer);
-
-            if (!["prisutan", "odsutan", "nijeUneseno"].includes(payload.statusPrisustva)) {
-                res.writeHead(201, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify({ status: `Status prisustva nije ispravan!` }));
+    else {
+        await db.predmet.findOne({
+            where: {
+                kod: payload.kod
+            }
+        }).catch(err => {
+            console.log("error -> db.predmet.findOne: " + err);
+        }).then(predmetPostoji => {
+            if (predmetPostoji) {
+                res.status(209).json({ status: `Predmet sa kodom ${payload.kod} već postoji` });
             }
             else {
-                {
-                    fs.readFile('./archives/predmeti.csv', function (err, data) {
-                        if (err) throw err;
+                db.predmet.create({
+                    naziv: payload.naziv,
+                    kod: payload.kod
+                });
+                res.status(200).json({ status: "Kreiran predmet!" });
+            }
+        }).catch(err => {
+            console.log("error -> db.predmet.create: " + err);
+        });
+    }
+});
 
-                        if (!data.includes(`,${payload.kodPredmeta}`)) {
-                            res.writeHead(201, { 'Content-Type': 'application/json' });
-                            res.end(JSON.stringify({ status: `Kod predmeta ne postoji!` }));
-                        }
-                    })
-                }
-                {
-                    fs.readFile('./archives/studenti.csv', function (err, data) {
-                        if (err) throw err;
-
-                        if (!data.includes(`,${payload.indexStudenta}\n`)) {
-                            res.writeHead(201, { 'Content-Type': 'application/json' });
-                            res.end(JSON.stringify({ status: `Student ne postoji!` }));
-                        }
-                    })
-                }
-                {
-                    fs.readFile('./archives/prisustva.csv', function (err, data) {
-                        if (err) throw err;
-                        var noviUpis = []
-                        if (data.includes(`${payload.tipCasa},${payload.redniBrojCasa},${payload.sedmica},${payload.kodPredmeta},${payload.indexStudenta},`)) {
-                            for (var red of data.toString().split('\n')) {
-                                red = (red.includes(`${payload.tipCasa},${payload.redniBrojCasa},${payload.sedmica},${payload.kodPredmeta},${payload.indexStudenta},`)) ? red.replace(/odsutan(?!,)|prisutan(?!,)|nijeUneseno(?!,)/gm, payload.statusPrisustva) : red
-                                noviUpis.push(red)
-                            }
-                            Upis = '';
-                            for (let red in noviUpis) { Upis += noviUpis[red] + ((red == noviUpis.length - 1) ? '' : '\n'); }
-                            fs.writeFile('./archives/prisustva.csv', Upis, (err) => {
-                                if (err) throw err;
-                            })
-                            res.writeHead(201, { 'Content-Type': 'application/json' });
-                            res.end(JSON.stringify({ status: `Izmjena napravljena!` }));
-                        }
-                        else {
-                            fs.appendFile('./archives/prisustva.csv', `${payload.tipCasa},${payload.redniBrojCasa},${payload.sedmica},${payload.kodPredmeta},${payload.indexStudenta},${payload.statusPrisustva}\n`, function (err) {
-                                if (err) throw err;
-                                res.writeHead(201, { 'Content-Type': 'application/json' });
-                                res.end(JSON.stringify({ status: "Kreirano prisustvo!" }));
-                            });
-                        }
-                    })
-                }
+app.post('/prisustvo', async (req, res) => {
+    const payload = req.body;
+    if (!["prisutan", "odsutan", "nijeUneseno"].includes(payload.statusPrisustva)) {
+        res.status(400).send({ status: "Status prisustva nije ispravan" });
+    }
+    else {
+        var daLiImaPredmet = await db.predmet.findOne({
+            where: {
+                kod: payload.kodPredmeta
             }
         });
-    }
-
-    else if (req.url.includes('/prisustvo') && req.method.toUpperCase() == 'GET') {
-        let buffer = '';
-        req.on('data', function (data) {
-            buffer += data;
-        });
-        req.on('end', function () {
-            const params = url.parse(req.url, true).query
-            var rprisutan = rodsutan = rnijeUneseno = 0;
-            fs.readFile('./archives/prisustva.csv', function (err, data) {
-                if (err) throw err;
-                for (var red of data.toString().split('\n')) {
-                    var podaciURedu = red.split(',')
-                    if (podaciURedu[2] == params.sedmica && podaciURedu[3] == params.kodPredmeta && podaciURedu[4] == params.indexStudenta) {
-                        if (podaciURedu[5].toString().includes("prisutan")) rprisutan++
-                        if (podaciURedu[5].toString().includes("odsutan")) rodsutan++
-                        if (podaciURedu[5].includes("nijeUneseno")) rnijeUneseno++
+        if (!daLiImaPredmet) {
+            res.status(400).send({ status: "Kod predmeta ne postoji!" });
+        }
+        else {
+            var daLiImaStudent = await db.student.findOne({
+                where: {
+                    index: payload.indexStudenta
+                }
+            });
+            if (!daLiImaStudent) {
+                res.status(400).send({ status: "Student ne postoji!" });
+            }
+            else {
+                await db.prisustvo.findOne({
+                    where: {
+                        '$ca.tip$': payload.tipCasa,
+                        '$ca.redniBroj$': payload.redniBrojCasa,
+                        '$ca.sedmica$': payload.sedmica,
+                        '$student.index$': payload.indexStudenta
+                    },
+                    include: [{
+                        model: db.cas,
+                        as: "ca",
+                        include: [{
+                            model: db.predmet,
+                            where: {
+                                kod: payload.kodPredmeta
+                            }
+                        }]
+                    },
+                    {
+                        model: db.student
+                    }]
+                }
+                ).then(prisustvo => {
+                    if (prisustvo) {
+                        prisustvo.status = payload.statusPrisustva;
+                        prisustvo.save();
+                        res.status(200).send({ status: "Azurirano prisustvo!" });
                     }
-                }
-                if (!rprisutan && !rodsutan && !rnijeUneseno) {
-                    res.writeHead(201, { 'Content-Type': 'application/json' });
-                    res.end(JSON.stringify({ status: "Prisustvo ne postoji!" }));
-                }
-                else {
-                    res.writeHead(201, { 'Content-Type': 'application/json' });
-                    res.end(JSON.stringify({ prisustvoZaSedmicu: params.sedmica, prisutan: rprisutan, odsutan: rodsutan, nijeUneseno: rnijeUneseno }));
-                }
-            })
-        });
+                    else {
+                        var pPred, pStud;
+                        return db.predmet.findOne({
+                            where: {
+                                kod: payload.kodPredmeta
+                            }
+                        }).then(predmet => {
+                            pPred = predmet;
+                            return db.student.findOne({
+                                where: {
+                                    index: payload.indexStudenta
+                                }
+                            });
+                        }).then(student => {
+                            pStud = student;
+                            student.addPredmeti(pPred, { through: db.studentPredmet });
+                            return db.cas.create({
+                                redniBroj: payload.redniBrojCasa,
+                                tip: payload.tipCasa,
+                                sedmica: payload.sedmica,
+                                predmetId: pPred.id
+                            });
+                        }).then(noviCas => {
+                            db.prisustvo.create({
+                                status: payload.statusPrisustva,
+                                studentId: pStud.id,
+                                casId: noviCas.id
+                            });
+                            res.status(200).send({ status: "Kreirano prisustvo!" });
+                        });
+                    }
+                });
+            }
+        }
     }
-}).listen(8080);
-console.log("slusam port 8080");
+});
+
+app.get('/prisustvo', async (req, res) => {
+    const params = req.query;
+    var filterStatusPrisustva = (statusP) => {
+        return {
+            where: {
+                '$ca.sedmica$': params.sedmica,
+                '$student.index$': params.indexStudenta,
+                status: statusP
+            },
+            include: [{
+                model: db.cas,
+                as: "ca",
+                include: [{
+                    model: db.predmet,
+                    where: {
+                        kod: params.kodPredmeta
+                    }
+                }]
+            },
+            {
+                model: db.student,
+            }]
+        };
+    };
+    var prisutan, odsutan, nijeUneseno;
+    prisutan = await db.prisustvo.count(filterStatusPrisustva("prisutan")).then(
+        odsutan = await db.prisustvo.count(filterStatusPrisustva("odsutan")).then(
+            nijeUneseno = await db.prisustvo.count(filterStatusPrisustva("nijeUneseno")).catch(err => {
+                console.log("error -> nijeUneseno: " + err);
+            })
+        ).catch(err => {
+            console.log("error -> odsutan: " + err)
+        })).catch(err => {
+            console.log("error -> prisutan: " + err);
+        });
+    if (!prisutan && !odsutan && !nijeUneseno) {
+        res.status(400).send({ status: "Prisustvo ne postoji!" });
+    }
+    else {
+        res.status(200).send({ prisustvoZaSedmicu: params.sedmica, prisutan: prisutan, odsutan: odsutan, nijeUneseno: nijeUneseno });
+    }
+});
+
+app.listen(8080, () => {
+    db.sequelize.sync({
+        force: true
+    });
+});
+console.log("listening to port 8080");
